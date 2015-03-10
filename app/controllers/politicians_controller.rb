a530a1f773cea78063a3d601d1d3cc495d861b95
rymcmahon <toedragdangle9@gmail.com>
2015-03-09 21:27:13 -0500

    added show page

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -6,4 +6,11 @@ class PoliticiansController < ApplicationController
 		#each loop around table
 	end
 
+	def show
+		@politician = Politician.find(params[:id])
+		@first_name = params[:first_name]
+		@last_name = params[:last_name]
+		@state = params[:state]
+	end
+
 end

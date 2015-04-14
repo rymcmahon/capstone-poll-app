6a00f69301dc6a435515660d5022306654675b3a
rymcmahon <toedragdangle9@gmail.com>
2015-04-14 15:05:29 -0500

    added third graph

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -24,15 +24,10 @@ class PoliticiansController < ApplicationController
 			donations << @politician_donors.industries[counter]["@attributes"]["total"]
 		end 
 
-		@chart = LazyHighCharts::HighChart.new('graph') do |f|
+		@one_column = LazyHighCharts::HighChart.new('graph') do |f|
 		  f.title(:text => "Top Donors by Industry")
 		  f.xAxis(:categories => industry_names)
 		  f.series(:name => "Donations Received", :yAxis => 0, :data => donations.map(&:to_i), :color => '#B00200')
-
-		  # f.yAxis [
-		  #   {:title => {:text => "Donations Received", :margin => 70} },
-		  
-		  # ]
 
 		  f.chart({:defaultSeriesType=>"column"})
 			end
@@ -50,22 +45,28 @@ class PoliticiansController < ApplicationController
 		total_contributions << @politician_donors.contributors[counter]["@attributes"]["total"]
 		end
 
-		@three_bar = LazyHighCharts::HighChart.new('graph') do |f|
+		@three_column = LazyHighCharts::HighChart.new('graph') do |f|
       f.title({ :text=>"Top Contributors by Organization"})
-      f.options[:xAxis][:categories] = contributor_names
-      # f.labels(:items=>[:html=>"Total fruit consumption", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])      
+      f.options[:xAxis][:categories] = contributor_names      
       f.series(:type=> 'column',:name=> 'Individuals',:data=> individual_contributions.map(&:to_i), :color => '#3A5C82')
       f.series(:type=> 'column',:name=> 'PACs',:data=> pacs_contributions.map(&:to_i), :color => '#33981C')
       f.series(:type=> 'column', :name=> 'Total',:data=> total_contributions.map(&:to_i), :color => '#DD7D06')
-      # f.series(:type=> 'spline',:name=> 'Average', :data=> [3, 2.67, 3, 6.33, 3.33])
-      # f.series(:type=> 'pie',:name=> 'Total consumption', 
-      #   :data=> [
-      #     {:name=> 'Jane', :y=> 13, :color=> 'red'}, 
-      #     {:name=> 'John', :y=> 23,:color=> 'green'},
-      #     {:name=> 'Joe', :y=> 19,:color=> 'blue'}
-      #   ],
-      #   :center=> [100, 80], :size=> 100, :showInLegend=> false)
     end
-		end
 
+    total = []
+		spent = []
+		cash_on_hand = []
+
+		total << @politician_donors.total["total"]
+		spent << @politician_donors.total["spent"]
+		cash_on_hand << @politician_donors.total["cash_on_hand"]
+
+		@three_bar = LazyHighCharts::HighChart.new('graph') do |f|
+		  f.title({:text => "Senator's Financial Summary"})
+		  f.options[:xAxis][:categories] = ["Receipts for 2014"]
+		  f.series(:type=> 'column', :name => "Total", :yAxis => 0, :data => total.map(&:to_i), :color => '#B00200')
+		  f.series(:type=> 'column', :name => "Spent", :yAxis => 0, :data => spent.map(&:to_i), :color => '#48A3F5')
+		  f.series(:type=> 'column', :name => "Cash on Hand", :yAxis => 0, :data => cash_on_hand.map(&:to_i), :color => '#545656')
+		end
+	end
 end
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,18 +1,21 @@
 class Donor
 
-	attr_reader :industries, :contributors
+	attr_reader :industries, :contributors, :total
 
-	def initialize(industry_hash, contributor_hash)
+	def initialize(industry_hash, contributor_hash, total_hash)
 		@industries = industry_hash["response"]["industries"]["industry"]
 		@contributors = contributor_hash["response"]["contributors"]["contributor"]
+		@total = total_hash["response"]["summary"]["@attributes"]
 	end
 
 	def self.find(cid)
 		politician_hash = Unirest.get("http://www.opensecrets.org/api/?method=candIndustry&cid=#{cid}&cycle=2014&apikey=#{ENV['opensecrets_api_key']}&output=json").body
 
 		contributor_hash = Unirest.get("http://www.opensecrets.org/api/?method=candContrib&cid=#{cid}&cycle=2014&apikey=#{ENV['opensecrets_api_key']}&output=json").body
+
+		total_contributions_hash = Unirest.get("http://www.opensecrets.org/api/?method=candSummary&cid=#{cid}&cycle=2014&apikey=#{ENV['opensecrets_api_key']}&output=json").body
 		
-    return Donor.new(politician_hash, contributor_hash)
+    return Donor.new(politician_hash, contributor_hash, total_contributions_hash)
 	end
 
 end

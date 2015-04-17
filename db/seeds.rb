ccdde692fed11d32212e51032fd6a5147abde1ae
rymcmahon <toedragdangle9@gmail.com>
2015-04-17 13:16:33 -0500

    fixed dw nominate spelling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+politicians = Politician.all
 
+	politicians.each do |politician|
+		request = Unirest.get("http://api.nytimes.com/svc/politics/v3/us/legislative/congress/members/#{politician.bio_guide}.json?api-key=df714ef37218ffaab38d3ac293fe9e6a%3A16%3A71448432").body
+
+		
+		sleep 1
+		
+		politician.update(:bills_sponsored => request["results"][0]["roles"][0]["bills_sponsored"], :bills_cosponsored => request["results"][0]["roles"][0]["bills_cosponsored"])
+		
+	end
 
 

0d93c35750d318d9c19c6cf96da78e3bcb375dfd
rymcmahon <toedragdangle9@gmail.com>
2015-04-21 20:22:36 -0500

    restored production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
-  config.assets.compile = true
 end
